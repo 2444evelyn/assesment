c93fe92f1cd0a9d584a7826923a4a78142151e37
@2444evelyn <everlinetsuma@gmail.com>
2022-03-10 09:15:42 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    details("codeHive")
+    println(details("Hive"))
+    modules(27,4)
+
+}
+fun details(name:String) {
+    var name = "codehive"
+     return
+}
+fun modules(num1:Int,num2:Int) {
+    var result = 1 % 2
+    println(result)
+}
